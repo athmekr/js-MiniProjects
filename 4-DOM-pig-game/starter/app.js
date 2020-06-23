d28dbdbe5c5869342b111136689bc3bfebbd49a5
athmekr <athmekr@gmail.com>
2020-06-23 14:43:05 +0300

    starting score and dice display integration

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -15,10 +15,17 @@ var scores, roundscore, activePlayer, dice;
 
 scores = [0,0];
 roundscore = 0;
-activePlayer = 0;
+activePlayer = 1;
 
 dice = Math.floor(Math.random()*6) +1;
 
 console.log(dice);
 
-document.querySelector('#current-0').textContent = dice;
+document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+var x = document.querySelector('#score-0').textContent;
+
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
